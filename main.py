19fef4959ecf571fbf911eb6d8fe6b2965584410
Toygar Birli <67593880+birlitoygar@users.noreply.github.com>
2023-03-25 06:19:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+headphones_cost = int(input('How much did you pay for your headphones? '))
+
+amazon_tax = (1 / 5) * headphones_cost  # 20% of something is equal to 1/5
+
+print(f"Yout amazon tax is {amazon_tax}")
